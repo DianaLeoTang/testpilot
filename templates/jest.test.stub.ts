47bdee7b1826e03aa159440fef8bf05d3d35a027
19801191517 <tangwangyao0228@163.com>
2025-06-11 22:25:29 +0800

    feat: add template example

---
+++ templates/jest.test.stub.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Diana Tang
+ */
+import { describe, it, expect } from '@jest/globals';
+import { $MODULE_NAME } from '$MODULE_PATH';
+
+describe('$MODULE_NAME', () => {
+  it('should work correctly', () => {
+    // TODO: Fill with actual tests
+    expect(true).toBe(true);
+  });
+});
